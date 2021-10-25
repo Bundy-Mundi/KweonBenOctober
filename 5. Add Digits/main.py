5db4ed8b4eed8c97dcf0cfc9f3256db01d280e5b
Ben Kweon <bennie.ben095@gmail.com>
2021-10-25 09:21:01 -0700

    Create main.py

---
+++ 5. Add Digits/main.py
@@ -0,0 +1,12 @@
+import math
+class Solution:
+    def addDigits(self, num: int) -> int:
+        total = 0
+        digits = 1
+        if num != 0: digits = math.ceil(math.log(num,10))
+        if num < 10: return num
+        for i in range(0, digits+1):
+            print(num)
+            total += num%10
+            num = num//10  
+        return self.addDigits(total)
